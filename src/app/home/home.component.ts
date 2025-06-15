4ee150bfc4ccd5a1959a827b36b1ef59926ce388
ramesshcoder <rameshkumarer321@gmail.com>
2025-06-15 16:34:10 +0530

    new commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AboutComponent } from "../about/about.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
