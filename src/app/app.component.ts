b6a975d9693758673534b22d8f52054c9554a34e
ramesshcoder <rameshkumarer321@gmail.com>
2025-06-15 15:59:36 +0530

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from './home/home.component';
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     {
         path:'',component:HomeComponent
+    },
+    {
+        path:'3',component:HomeComponent
+    },
+    {
+        path:'About',component:AboutComponent
+    }
+    ,
+    {
+        path:'education',component:HomeComponent
+    },
+    {
+        path:'3',component:HomeComponent
     },
     {
         path:'3',component:HomeComponent
