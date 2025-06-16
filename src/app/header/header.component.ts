837ceb4af0e5d4664c92096c4ff88b613c2f5a87
ramesshcoder <rameshkumarer321@gmail.com>
2025-06-16 22:40:22 +0530

    update commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink,],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
